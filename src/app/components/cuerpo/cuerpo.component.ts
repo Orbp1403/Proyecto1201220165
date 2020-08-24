3018ed0502b0218655308fe8ec9dfc6fa3bee2b4
201220165Orbp <2492944860101@ingenieria.usac.edu.gt>
2020-08-24 16:29:13 -0600

    Quitando funciones que no hacen nada

--- src/app/components/cuerpo/cuerpo.component.ts
+++ src/app/components/cuerpo/cuerpo.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, ɵSWITCH_CHANGE_DETECTOR_REF_FACTORY__POST_R3__ } from '@angular/core';
+import { Component, OnInit, ɵSWITCH_CHANGE_DETECTOR_REF_FACTORY__POST_R3__, EventEmitter } from '@angular/core';
 import * as $ from 'jquery';
+import { CompileSummaryKind } from '@angular/compiler';
 
 @Component({
   selector: 'app-cuerpo',
@@ -12,47 +13,23 @@ export class CuerpoComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    var pos = document.getElementById("labelpos");
-    pos.textContent = "Columna: 0, Fila: 0";
     $(document).ready(function () {
-      $("#txtcod").bind("keydown keypress", function (e) {
-          var cursorPosition = $("#txtcod").prop("selectionStart");
-          console.log(cursorPosition);
+      $("#text-area").bind("keydown keypress", function () {
+        var cursorPosition = $("#text-area").prop("selectionStart");
+        console.log(cursorPosition);
       }).trigger('propertychange');;
+    
+      // Target all classed with ".lined"
+      $(".lined").linedtextarea(
+        {selectedLine: 1}
+      );
+    
+      // Target a single one
+      $("#text-area").linedtextarea();
+    
     });
-  }
-
-  onchange(value : string){
-    var ultimatecla = value[value.length - 1];
-    console.log(ultimatecla);
-    if(ultimatecla != undefined){
-      //para separar el label
-      var pos = document.getElementById("labelpos");
-      var contenidopos = pos.textContent;
-      var auxcontenidopos = contenidopos.split(',');
-      var columna = +auxcontenidopos[0].split(': ')[1];
-      var fila = +auxcontenidopos[1].split(': ')[1];
-      //termina de separa el label
-      //separa el contenido
-      var contenido = value.split('\n');
-      console.log(contenido);
-      //termina de separar el contenido
-      if(ultimatecla == '\n'){
-        console.log("es enter");
-        fila = contenido.length
-        console.log(contenido[contenido.length - 1]);
-        var auxcol = contenido[contenido.length - 1]
-        columna = auxcol.length;
-      }else{
-        console.log("no es enter");
-        fila = contenido.length
-        var auxcol = contenido[contenido.length - 1]
-        columna = auxcol.length;
-        console.log(columna);
-      }
-      console.log(fila);
-      console.log(columna);
-      pos.textContent = "Columna: " + columna + ", Fila: " + fila;
-    }
+    
+    
+    //TODO posicion del cursor
   }
 }
